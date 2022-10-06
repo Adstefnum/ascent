76d343c0c75456dfad1aceb9bbd8a925fb839a6f
Stephen Adesina <bauerstevenson@gmail.com>
2022-10-06 07:09:42 +0300

    fixing tailwind

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.static_folder = 'static'
 
 def index():
     return render_template("index.html")
