08817c2cc1d6c62257dd0befae1b505bfca04036
Stephen Adesina <bauerstevenson@gmail.com>
2022-10-06 14:43:50 +0300

    fixed imports

--- src/ui/routes.py
+++ src/ui/routes.py
@@ -1,7 +1,7 @@
-from src.ui.app import app
-from src.ui.app import index
-from src.utils.upload_file import upload_file
-from src.utils.get_data_from_database import get_data_from_database
+from ui import app
+from .views.index import index
+from utils.upload_file import upload_file
+from utils.get_data_from_database import get_data_from_database
 
 app.add_url_rule('/', view_func=index)
 app.add_url_rule('/upload-file', view_func=upload_file,
