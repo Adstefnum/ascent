a54382ab3566062c53aa9678c120796028b6c9af
Adstefnum <bauerstevenson@gmail.com>
2022-10-08 22:02:48 +0300

    upload functionality

--- src/utils/upload_file.py
+++ src/utils/upload_file.py
@@ -1,5 +1,10 @@
-def upload_file(file_ext):
+from flask import request, Response
+import os
+from ui import app
+
+def upload_file():
     if request.method == 'POST':
+        file_ext = request.args.to_dict().get("file_ext")
         file = request.files['file']
-        file.save(f"../../uploads/upload.{file_ext}")
-    return 200
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], f"upload.{file_ext}"))
+    return Response("{'msg':'success'}", status=200, mimetype='application/json')
