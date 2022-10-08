e0fab236d30cf798c3ef10289a9cc7bdaa441cd7
Adstefnum <bauerstevenson@gmail.com>
2022-10-08 16:57:16 +0300

    file upload util

--- src/utils/upload_file.py
+++ src/utils/upload_file.py
@@ -1,2 +1,5 @@
-def upload_file():
-    pass
+def upload_file(file_ext):
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(f"../../uploads/upload.{file_ext}")
+    return 200
