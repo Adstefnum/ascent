d94a95b2bf94519d69737f245deffb6c4272b3d2
Adstefnum <bauerstevenson@gmail.com>
2022-10-09 06:18:07 +0300

    adding network and app safety

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,6 +1,9 @@
 
 
-from flask import Flask
+from flask import Flask, session
+from flask_cors import CORS
+from flask_session import Session
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
@@ -8,3 +11,12 @@ from . import routes
 app.static_folder = 'static'
 UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+
+csrf = CSRFProtect()
+Session(app)
+CORS(app)
+
+@app.template_filter('caps')
+def reverse_filter(string:str):
+    return string.upper()
+
