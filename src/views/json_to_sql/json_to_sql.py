51c5e5c01a84979be44bf8968615b822cb925830
Adstefnum <bauerstevenson@gmail.com>
2022-10-12 06:30:16 +0300

    building json to sql upload backend

--- src/views/json_to_sql/json_to_sql.py
+++ src/views/json_to_sql/json_to_sql.py
@@ -1,9 +1,42 @@
-from flask import render_template
+from flask import render_template,request,Response
 from forms.json_to_sql import JsonToDatabaseForm
+from utils.dbutils import get_connection
+from utils.file_upload_utils import user_upload_file
 
 def json_to_sql():
-    form = JsonToDatabaseForm()
-    form.validate()
-    #after validation then I need to connect to the database, start processing the json into 
-    #transactions and then upload at once.
-    return render_template('json_to_sql/index.html',file_ext="json",form=form)
+    
+    if request.GET:
+        form = JsonToDatabaseForm()
+        return render_template('json_to_sql/index.html',file_ext="json",form=form)
+    if request.POST and form.validate():
+        data = form.to_dict()
+        user_upload_file("json")
+        return json_to_sql_upload(data)
+    return Response("{'msg':'failure'}", status=404, mimetype='application/json')
+
+
+def json_to_sql_upload(data ):
+    
+    conn = get_connection(data)
+    cur = conn.cursor()
+
+    transactions = []
+
+    create_table_sql = """
+    """
+
+    insert_sql = """
+         
+        """ 
+
+
+
+    file = os.path.join(app.config["UPLOAD_FOLDER"], "upload.json")
+    
+    with open(file, 'r') as f:
+        data = ujson.loads(f.read())
+
+
+
+
+    return Response("{'msg':'success'}", status=200, mimetype='application/json')
