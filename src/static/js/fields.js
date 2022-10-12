0802920a8d66b5ffc0c76fe9a011dae307d289bc
Adstefnum <bauerstevenson@gmail.com>
2022-10-12 05:33:41 +0300

    filterinf fields

--- src/static/js/fields.js
+++ src/static/js/fields.js
@@ -4,8 +4,12 @@
         $('#use_tcp').click(function(e){
             if ($('#use_tcp').prop('checked')){
                 $('#unix_socket').parent().hide();
+                $("#host").parent().show();
+                $("#port").parent().show();
             }else{
                 $("#unix_socket").parent().show();
+                $("#host").parent().hide();
+                $("#port").parent().hide();
             }
         });
     });
